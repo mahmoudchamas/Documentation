f2400f83825dff2d61eff6082da14309732591a1
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-07 12:32:30 +0200

    pushing step 6

--- 03_Exercises/03_JavaScript/javascript-basics-01/Step_5.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_5.js
@@ -4,4 +4,4 @@ var num2 = prompt("enter the seconde number") ;
 
 
 var num3=num1*num2;
-window.alert( "num1 x num2= "+parseInt(num3));
+window.alert( "num1 x num2=  "+parseInt(num3));
---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+var num1 = prompt("enter the first number") ;
+var num2 = prompt("enter the seconde number") ;
+
+
+
+var num3=num1%num2;
+window.alert( "the remainder of the division: "+parseInt(num3));
