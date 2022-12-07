7d450e7717d5c34cf4784ac50ee51e6388e6451c
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-07 11:09:50 +0200

    pushing step 3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let name1 = prompt("what is your name?") ;
+let surname = prompt("what is your name?") ;
+let city = prompt("what is your name?") ;
+alert("name:"+name1+"\n"+"surname:"+surname+"\n"+"city:"+city    )
