2042686b56434c3e55e345b213f2b454c47e3d2b
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-07 15:12:49 +0200

    pushing step8

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_8.js
@@ -0,0 +1,8 @@
+let age = prompt("enter your age");
+
+if(age <= 18){
+    alert("your age is ander 18");
+}else{
+    alert("your age is over 18");
+ 
+}
---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_3 copy.js
@@ -0,0 +1,7 @@
+let person = prompt("enter your name");
+let text ;
+if(person==null || person ==""){
+    text="user cancelled the prompt.";
+}else{
+  window.alert("hello"+"  "+person);
+}
