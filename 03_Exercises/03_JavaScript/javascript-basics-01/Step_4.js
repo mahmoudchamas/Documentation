45c9ec44d81532e951ded54120e3e131a002486b
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-07 20:51:01 +0200

    update step_4

--- 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -1,2 +1,10 @@
-
-alert("name:"+name1+"\n"+"surname:"+surname+"\n"+"city:"+city    )
+alert("enter your name, surname and city")
+function info(){
+    var num1 , num2 , sum ;
+    name1 = parseInt(document.getElementById("name").value);
+    surname= parseInt(document.getElementById("surname").value);
+    city= parseInt(document.getElementById("city").value);
+    
+    
+alert("name:"+name1+"\n"+"surname:"+surname+"\n"+"city:"+city    );
+}
