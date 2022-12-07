e69039b74a48b11b2830ba7ad736b5eba3504663
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-07 10:18:29 +0200

    pushing step3

--- 03_Exercises/03_JavaScript/javascript-basics-01/step_2.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_2.js
@@ -1,5 +1,5 @@
 var name1="Mahmoud";
 var surname="Chamas";
 var city="Beirut";
-alert("name :"+name1+"\n"+"surname:"+surname+"\n"+"city:"+city    )
+alert("name:"+name1+"\n"+"surname:"+surname+"\n"+"city:"+city    )
 
