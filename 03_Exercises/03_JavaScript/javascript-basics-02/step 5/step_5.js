c15e5090a52c75045dc81482a6edcea33cf1175f
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-08 23:43:16 +0200

    push step5

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,37 @@
+function changeImage(){
+    const image1 = document.querySelector("#image1");
+    
+    image1.addEventListener('mouseover', function (){
+        image1.src="images/image1_2.jpg"
+        
+    })
+  
+        const image2 = document.querySelector("#image2")
+
+    image2.addEventListener('mouseover', function (){
+        image2.src="images/image2_2.jpg"
+        
+    })
+        const image3 = document.querySelector("#image3");
+    
+    image3.addEventListener('mouseover', function (){
+        image3.src="images/image3_2.jpg"  
+    })
+
+    const image4 = document.querySelector("#image4");
+
+       image4.addEventListener('mouseover', function (){
+        image4.src="images/image4_2.jpg"
+        
+    })
+
+        const image5 = document.querySelector("#image5");
+
+    image5.addEventListener('mouseover', function (){
+        image5.src="images/image5_2.jpg"
+        
+    })
+ 
+    
+  
+}
