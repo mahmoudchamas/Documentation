dd9ad5570863e6e1cc61b61754dd9497ba6dbfcc
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-08 22:20:20 +0200

    pushing step 2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,8 @@
+function visibl(id){
+    var e = document.getElementById(id);
+    if(e.style.display == "show"){
+        e.style.display = 'block';
+    }else if(e.style.display == "hide")
+        e.style.display = 'hide';
+    }
+
