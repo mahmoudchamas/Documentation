ee9c3a325724bc18d97ddcd0e16a4c5c9b855362
mahmoudchamas <mahmoudchamas47@gmail.com>
2022-12-11 19:09:38 +0200

    creating table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -6,3 +6,15 @@ SELECT Points FROM students Where (Name == 'Alex');
 INSERT INTO students VALUES (7,'Mahmoud Chamas' , 29 , 'M',300); 
 UPDATE   students SET Points = 400 WHERE Name = 'Basma';
 UPDATE   students SET Points = 100 WHERE Name = 'Alex';
+ CREATE TABLE "graduates"(
+       "ID"  INTEGER Not NULL ,
+	   "Name" TEXT NOT NULL UNIQUE ,
+	   "Age" TEXT ,
+	   "Gender" TEXT,
+	   Points TEXT,
+	   "Graduation" text,
+	   PRIMARY key ("ID" AUTOINCREMENT)
+	   );
+SELECT * from students ;
+	   INSERT INTO graduates VALUES (1,'Layal' , 18 , 'M',300, 08/09/2018) ; 
+	   DELETE  FROM students WHERE (name = 'layla');
