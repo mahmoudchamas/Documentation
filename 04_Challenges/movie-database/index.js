84d610dc99c26d6e4670a4210f93cf4e51ae996e
mahmoudchamas <mahmoudchamas47@gmail.com>
2023-01-06 22:33:31 +0200

    step 3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,12 +1,27 @@
-const http = require('http');
+"use strict";
+const express = require("express");
+const port = 3000;
+const app = express();
+
+const http = require("http");
 
 const server = http.createServer();
-server.on('request',(req, res)=> {
-    res.end('welcome');
-})
+server.on("request", (req, res) => {
+  res.end("welcome");
+});
 
-server.listen(3000 , () => {
+app.listen(3000, () => {
+  console.log("OK : server listing on port : 3000");
+});
 
-console.log('OK : server listing on port : 3000');
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
 
-})
+app.get("/time", (req, res) => {
+  let date1 = new Date();
+  let hours = date1.getHours();
+  let minutes = date1.getMinutes();
+  res.send({ status: 200, message: hours + " :" + minutes });
+});
+
